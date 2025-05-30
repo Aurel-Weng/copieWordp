e9e0f6ca1cbd55f487d473f375bb2cf0bd828dce
Aurelien Lefebvre <devbyaurel@gmail.com>
2025-05-30 13:52:58 +0200

    add_bdd à jour avec contest/forminator, manip -> il manque contest

---
+++ add_bdd.php
@@ -0,0 +1,458 @@
+<?php
+
+function add_users($bdd, $dossier) {
+    $reqPushUser = "LOAD DATA INFILE '/var/lib/mysql-files/$dossier/old_users.csv'
+        INTO TABLE `hr8qI_users`
+        FIELDS TERMINATED BY ',' 
+        OPTIONALLY ENCLOSED BY '\"'
+        LINES TERMINATED BY '\\n'
+        IGNORE 1 ROWS
+        (@ID, `user_login`, `user_pass`, `user_nicename`, `user_email`, `user_url`, `user_registered`, `user_status`, `display_name`)
+        SET user_login = user_login,
+            user_pass = user_pass,
+            user_nicename = user_nicename,
+            user_email = user_email,
+            user_url = user_url,
+            user_registered = user_registered,
+            user_activation_key = '',
+            user_status = user_status,
+            display_name = display_name";
+
+    $bdd->exec($reqPushUser);
+}
+
+function get_users($bdd, $dossier){
+    $reqGetUsers = "SELECT id, user_email 
+        FROM `hr8qI_users`
+        INTO OUTFILE '/var/lib/mysql-files/$dossier/new_users.csv'
+        FIELDS TERMINATED BY ','
+        LINES TERMINATED BY '\\n'";
+        //suppression WHERE id > 13
+    
+    $bdd->exec($reqGetUsers);
+}
+
+function get_contest($bdd, $dossier) {
+    $reqGetContest = "SELECT *
+        FROM `hr8qI_posts`
+        WHERE `post_type` = 'contest'
+        INTO OUTFILE '/var/lib/mysql-files/$dossier/contest/new_post.csv'
+        FIELDS TERMINATED BY ','
+        LINES TERMINATED BY '\\n'";
+    $bdd->exec($reqGetContest);
+}
+
+function add_usermeta($bdd, $dossier){
+    // Change ids in usermeta
+    $old = fopen("/var/lib/mysql-files/$dossier/old_users.csv", 'r');
+    $meta = fopen("./$dossier/usermeta.csv", 'r');
+
+    $replace_id = [];
+
+    fgetcsv($old, null, ';');
+    fgetcsv($meta, null, ';');
+
+    while ($ligne_old = fgetcsv($old, null, ';')) {
+        $old_data = explode(',', $ligne_old[0]);
+        
+        $new = fopen("/var/lib/mysql-files/$dossier/new_users.csv", 'r');
+
+        while ($ligne_new = fgetcsv($new, null, ';')) {
+            $data = explode(',', $ligne_new[0]);
+
+            if (!strcmp($data[1],$old_data[4])) {
+            $replace_id[$old_data[0]] = $data[0];
+                break;
+            }
+        }
+        fclose($new);
+    }
+
+    // Rewrite usermeta with new IDs
+    echo "Réécriture du fichier meta avec les nouveaux IDs\n";
+    $output = fopen("/var/lib/mysql-files/$dossier/meta_user_updated.csv", 'w');
+    fputcsv($output, ["user_id","meta_key","meta_value"], ',');
+
+    while ($ligne_meta = fgetcsv($meta, null, ',')) {
+        if (isset($replace_id[$ligne_meta[0]])) {
+            $ligne_meta[0] = $replace_id[$ligne_meta[0]];
+        }
+        
+        // Replace Kg0768AR0
+        $ligne_meta[1] = str_replace("Kg0768AR0", "hr8qI", $ligne_meta[1]);
+
+        fputcsv($output, $ligne_meta, ',');
+    }
+
+    fclose($meta);
+    fclose($output);
+
+    // Add usermeta to database
+    echo "Ajout des usermeta à la base de données\n";
+    $reqPushUserMeta = "LOAD DATA INFILE '/var/lib/mysql-files/$dossier/meta_user_updated.csv'
+        INTO TABLE `hr8qI_usermeta`
+        FIELDS TERMINATED BY ',' 
+        ENCLOSED BY '\"'
+        LINES TERMINATED BY '\\n'
+        IGNORE 1 ROWS
+        (`user_id`, `meta_key`, `meta_value`)";
+    $bdd->exec($reqPushUserMeta);
+}
+
+function change_authorId($dossier){
+    $post = fopen("./$dossier/old_posts.csv", 'r');
+
+    $author_id = [];
+
+    fgetcsv($post, null, ';');
+
+    while ($ligne_post = fgetcsv($post, null, ';')) {
+        $post_data = explode(',', $ligne_post[0]);
+        if (!array_key_exists($post_data[1], $author_id)) {
+            
+            $old = fopen("/var/lib/mysql-files/$dossier/old_users.csv", 'r');
+            fgetcsv($old, null, ';');
+            
+            while ($ligne_old = fgetcsv($old, null, ';')) {
+                $old_data = explode(',', $ligne_old[0]);
+                
+                if (strcmp($old_data[0], $post_data[1]) == 0) {
+                    $new = fopen("/var/lib/mysql-files/$dossier/new_users.csv", 'r');
+
+                    while ($ligne_new = fgetcsv($new, null, ';')) {
+                        $data = explode(',', $ligne_new[0]);
+                
+                        if (!strcmp($data[1],$old_data[4])) {
+                            $author_id[$post_data[1]] = $data[0];
+                            break;
+                        }
+                    }
+                    fclose($new);
+                }
+                
+            }
+            fclose($old);
+        }
+    }
+    fclose($post);
+
+    $post = fopen("./$dossier/old_posts.csv", 'r');
+    fgetcsv($post, null, ';');
+    $output = fopen("/var/lib/mysql-files/$dossier/old_posts.csv", 'w');
+    fputcsv($output, ["post_author","post_date","post_date_gmt","post_content","post_title","post_excerpt","post_status","comment_status","ping_status","post_password","post_name","to_ping","pinged","post_modified","post_modified_gmt","post_content_filtered","post_parent","guid","menu_order","post_type","post_mime_type","comment_count"], ',');
+
+    while ($ligne_meta = fgetcsv($post, null, ',')) {
+        // Remplacer ID si trouvé
+        if (isset($author_id[$ligne_meta[1]])) {
+            $ligne_meta[1] = $author_id[$ligne_meta[1]];
+        }
+
+        // Écriture ligne par ligne avec virgule comme séparateur
+        fputcsv($output, $ligne_meta, ',');
+    }
+
+    fclose($post);
+    fclose($output);
+}
+
+function change_idPostMeta($dossier, $categorie){
+    if ($categorie == 'contest') {
+        $old = fopen("./$dossier/contest/old_posts.csv", 'r');
+        $new = fopen("/var/lib/mysql-files/$dossier/contest/new_post.csv", 'r');
+
+        // Output file for updated contest data
+        $output = fopen("/var/lib/mysql-files/$dossier/contest/new_posts.csv", 'w');
+        $meta = fopen("./$dossier/contest/old_fca_cc_activity_tbl.csv" , 'r');
+    } else {
+        $old = fopen("/var/lib/mysql-files/$dossier/old_posts.csv", 'r');
+        $new = fopen("/var/lib/mysql-files/$dossier/new_posts.csv", 'r');
+
+        $output = fopen("/var/lib/mysql-files/$dossier/new_posts.csv", 'w');
+        $meta = fopen("./$dossier/postmeta.csv", 'r');
+    }
+    $replace_id = [];
+
+    $first_meta_line = fgetcsv($meta, null, ';');
+
+    fgetcsv($old, null, ';');
+
+    while ($ligne_old = fgetcsv($old, null, ';')) {
+        $old_data = explode(',', $ligne_old[0]);
+
+        while ($ligne_new = fgetcsv($new, null, "\n")) {
+            $data = explode(',', $ligne_new[0]);
+ 
+            echo "Comparaison des données :\n";
+            echo "Ancien ID: " . $old_data[0] . "\n";
+            echo "Nouveau ID: " . $data[0] . "\n";
+
+            if ( strcasecmp(trim($data[2], '"'),trim($old_data[2], '"'))==0 && strcasecmp(trim($data[5], '"'),trim($old_data[5], '"'))==0 && strcasecmp(trim($data[11], '"'),trim($old_data[11], '"'))==0 ) {
+                $replace_id[$old_data[0]] = $data[0];
+                break;
+            }
+        }
+    }
+    fclose($new);
+
+    // Réécriture du fichier meta avec les nouveaux IDs
+    fputcsv($output, $first_meta_line, ',');
+
+    while ($ligne_meta = fgetcsv($meta, null, ',')) {
+        // var_dump($ligne_meta);
+        // Remplacer ID si trouvé
+        if (isset($replace_id[$ligne_meta[1]])) {
+            $ligne_meta[1] = $replace_id[$ligne_meta[1]];
+        }
+
+        // Écriture ligne par ligne avec virgule comme séparateur
+        fputcsv($output, $ligne_meta, ',');
+    }
+    fclose($meta);
+    fclose($output);
+}
+
+function add_posts($bdd, $dossier){
+    change_authorId($dossier);
+
+    $reqPushPost = "LOAD DATA INFILE '/var/lib/mysql-files/$dossier/old_posts.csv'
+        INTO TABLE `hr8qI_posts`
+        FIELDS TERMINATED BY ',' 
+        OPTIONALLY ENCLOSED BY '\"'
+        LINES TERMINATED BY '\\n'
+        IGNORE 1 ROWS
+        (@ID, `post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`)
+        SET post_author = post_author,
+            post_date = post_date,
+            post_date_gmt = post_date_gmt,
+            post_content = post_content,
+            post_title = post_title,
+            post_excerpt = post_excerpt,
+            post_status = post_status,
+            comment_status = comment_status,
+            ping_status = ping_status,
+            post_password = post_password,
+            post_name = post_name,
+            to_ping = to_ping,
+            pinged = pinged,
+            post_modified = post_modified,
+            post_modified_gmt = post_modified_gmt,
+            post_content_filtered = post_content_filtered,
+            post_parent = post_parent,
+            guid = guid,
+            menu_order = menu_order,
+            post_type = post_type,
+            post_mime_type = post_mime_type,
+            comment_count = comment_count";
+    
+    $bdd->exec($reqPushPost);
+}
+
+function get_posts($bdd, $dossier){
+    $reqGetPosts = "SELECT *
+        FROM `hr8qI_posts`
+        WHERE post_type = 'attachment'
+        INTO OUTFILE '/var/lib/mysql-files/$dossier/new_posts.csv'
+        FIELDS TERMINATED BY ','
+        LINES TERMINATED BY '\\n'";
+    
+    $bdd->exec($reqGetPosts);
+}
+
+function add_postmeta($bdd, $dossier){
+    change_idPostMeta($dossier, 'postmeta');
+
+    $reqPushPostMeta = "LOAD DATA INFILE '/var/lib/mysql-files/$dossier/postmeta.csv'
+        INTO TABLE `hr8qI_postmeta`
+        FIELDS TERMINATED BY ',' 
+        OPTIONALLY ENCLOSED BY '\"'
+        LINES TERMINATED BY '\\n'
+        IGNORE 1 ROWS
+        (@id, `post_id`, `meta_key`, `meta_value`)
+        SET post_id = post_id,
+            meta_key = meta_key,
+            meta_value = meta_value";
+    
+    $bdd->exec($reqPushPostMeta);
+}
+
+function add_date_forminator($dossier) {
+    $tab_fichier = [
+        "old_views.csv" => [
+            'bonne_date' => 5,
+            'up_date' => 6,
+        ],
+        "old_entry_meta.csv" => [
+            'bonne_date' => 4,
+            'up_date' => 5,
+        ],
+    ];
+
+    foreach ($tab_fichier as $fichier => $cases) {
+        $filename = "./$dossier/forminator/".$fichier;
+        $filetmp = "/var/lib/mysql-files/$dossier/".$fichier;
+        if (($handle = fopen($filename, 'r')) !== false && ($tempHandle = fopen($filetmp, 'w')) !== false) {
+            while (($data = fgetcsv($handle, 1000, ',', '"')) !== false) {
+                if ($data[$cases['up_date']] == '0000-00-00 00:00:00') {
+                    $data[$cases['up_date']] = $data[$cases['bonne_date']];
+                }
+                fputcsv($tempHandle, $data, ',', '"');
+            }
+            fclose($tempHandle);
+            fclose($handle);
+        } else {
+            echo "Impossible d'ouvrir le fichier.";
+        }
+    }
+}
+
+function add_data_forminator($bdd, $dossier) {
+    // delete old views
+    $reqSuppViews = "DELETE FROM `hr8qI_frmt_form_views`";
+    $bdd->exec($reqSuppViews);
+
+    // add new views
+    $reqAddNewViews = "LOAD DATA INFILE '/var/lib/mysql-files/$dossier/old_views.csv'
+        INTO TABLE `hr8qI_frmt_form_views`
+        FIELDS TERMINATED BY ',' 
+        OPTIONALLY ENCLOSED BY '\"'
+        LINES TERMINATED BY '\\n'
+        IGNORE 1 ROWS
+        (`view_id`, `form_id`, `page_id`, `ip`, `count`, `date_created`, `date_updated`)
+        SET view_id = view_id,
+            form_id = form_id,
+            page_id = page_id,
+            ip = ip,
+            count = count,
+            date_created = date_created,
+            date_updated = date_updated";
+    $bdd->exec($reqAddNewViews);
+
+    // delete old reports
+    $reqSuppReports = "DELETE FROM `hr8qI_frmt_form_reports`";
+    $bdd->exec($reqSuppReports);
+
+    // add new reports
+    $reqAddReports = "LOAD DATA INFILE '/var/lib/mysql-files/$dossier/old_reports.csv'
+        INTO TABLE `hr8qI_frmt_form_reports`
+        FIELDS TERMINATED BY ',' 
+        OPTIONALLY ENCLOSED BY '\"'
+        LINES TERMINATED BY '\\n'
+        IGNORE 1 ROWS
+        (`report_id`, `report_value`, `status`, `date_created`, `date_updated`)
+        SET report_id = report_id,
+            report_value = report_value,
+            status = status,
+            date_created = date_created,
+            date_updated = date_updated";
+    $bdd->exec($reqAddReports);
+
+    // delete old entry
+    $reqSuppEntry = "DELETE FROM `hr8qI_frmt_form_entry`";
+    $bdd->exec($reqSuppEntry);
+
+    // add new entry
+    $reqAddEntry = "LOAD DATA INFILE '/var/lib/mysql-files/$dossier/old_entry.csv'
+        INTO TABLE `hr8qI_frmt_form_entry`
+        FIELDS TERMINATED BY ',' 
+        OPTIONALLY ENCLOSED BY '\"'
+        LINES TERMINATED BY '\\n'
+        IGNORE 1 ROWS
+        (`entry_id`, `entry_type`, `draft_id`, `form_id`, `is_spam`, `date_created`)
+        SET entry_id = entry_id,
+            entry_type = entry_type,
+            draft_id = draft_id,
+            form_id = form_id,
+            is_spam = is_spam,
+            date_created = date_created";
+    $bdd->exec($reqAddEntry);
+
+    // delete old entry meta
+    $reqSuppEntryMeta = "DELETE FROM `hr8qI_frmt_form_entry_meta`";
+    $bdd->exec($reqSuppEntryMeta);
+
+    // add new entry meta
+    $reqAddEntryMeta = "LOAD DATA INFILE '/var/lib/mysql-files/$dossier/old_entry_meta.csv'
+        INTO TABLE `hr8qI_frmt_form_entry_meta`
+        FIELDS TERMINATED BY ',' 
+        OPTIONALLY ENCLOSED BY '\"'
+        LINES TERMINATED BY '\\n'
+        IGNORE 1 ROWS
+        (`meta_id`, `entry_id`, `meta_key`, `meta_value`, `date_created`, `date_updated`)
+        SET meta_id = meta_id,
+            entry_id = entry_id,
+            meta_key = meta_key,
+            meta_value = meta_value,
+            date_created = date_created,
+            date_updated = date_updated";
+    $bdd->exec($reqAddEntryMeta);
+}
+
+// Check if the script is run with the correct number of arguments
+if (!($argc = 6 && $argv[1] != " " && $argv[2] != " " && $argv[3] != " " && $argv[4] != " " && $argv[5] != " ")) {
+    echo "Usage: php add_bdd.php <option> <nom_site> <database_name> <username> <password>";
+    die();
+}
+
+// Connect to the database
+try {
+    $database = $argv[3];
+    $username = $argv[4];
+    $password = $argv[5];
+
+    // Create connection
+    $conn = new PDO("mysql:host=localhost:3306;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    die();
+}
+
+if ($argv[1] == "-a" || $argv[1] == "--all") {
+    echo "Adding users...\n";
+    add_users($conn, $argv[2]);
+
+    echo "Getting users...\n";
+    get_users($conn, $argv[2]);
+
+    add_usermeta($conn, $argv[2]);
+
+
+    echo "Adding posts...\n";
+    add_posts($conn, $argv[2]);
+
+    echo "Getting posts...\n";
+    get_posts($conn, $argv[2]);
+
+    echo "Adding postmeta...\n";
+    add_postmeta($conn, $argv[2]);
+}
+else if ($argv[1] == "-p" || $argv[1] == "--posts") {
+    echo "Adding posts...\n";
+    add_posts($conn, $argv[2]);
+
+    echo "Getting posts...\n";
+    get_posts($conn, $argv[2]);
+}
+else if ($argv[1] == "-u" || $argv[1] == "--user") {
+    echo "Adding users...\n";
+    add_users($conn, $argv[2]);
+
+    echo "Getting users...\n";
+    get_users($conn, $argv[2]);
+
+    add_usermeta($conn, $argv[2]);
+}
+else if ($argv[1] == "-f" || $argv[1] == "--forminator") {
+    echo "Change dates forminator...\n";
+    add_date_forminator($argv[2]);
+
+    echo "Adding data forminator...\n";
+    add_data_forminator($conn, $argv[2]);
+}
+else if ($argv[1] == "-c" || $argv[1] == "--contest") {
+    echo "Getting contest...\n";
+    // get_contest($conn, $argv[2]);
+
+    echo "Changing ID postmeta...\n";
+    change_idPostMeta($argv[2], 'contest');
+}
---
+++ manip.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+# This script is used to import and manipulate data from CSV  files to the new wordpress site.
+# Before running this script, make sure to follow the instructions below:
+# 1. Create a new database and user in your MySQL server.
+# 2. Grant the user all privileges on the new database.
+# 3. Create a new WordPress site and configure it to use the new database.
+# 4. Make sure to have the CSV files in the next directory : /var/lib/mysql-files/[NAME_SITE]
+# 5. Make sure you imported all users from the old site to the new site, same for the uploads directory.
+# END
+#
+# To run this script, use the following command:
+# ./manip.sh [OPTIONS] [NAME_SITE] [DB_NAME] [DB_USER] [DB_PASS]
+# Options:
+# -h, --help: Show this help message
+# -u, --user: To change the user_id of userspost table
+# -p, --post: To change the post
+# -a, --all: To change all the user_id and post_id
+
+
+# /var/lib/mysql-files/$NAME_SITE/
+
+
+# Help message
+if [ "$#" -eq 0 ] || ( [ "$#" -eq 1 ] && { [ "$1" == "-h" ] || [ "$1" == "--help" ]; } ); then
+    echo "Usage: $0 [OPTIONS] [NAME_SITE] [DB_NAME] [DB_USER] [DB_PASS]"
+    echo "Options:"
+    echo "-h, --help: Show this help message"
+    echo "-u, --user: To change the user_id and add all users data"
+    echo "-p, --post: To change the post and add all posts data"
+    echo "-f, --forminator: To change the post_id and add all forminator's data"
+    echo "-a, --all: To change and add all users, posts and forminator's data"
+    exit 0
+fi
+
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 [OPTIONS] [NAME_SITE] [DB_NAME] [DB_USER] [DB_PASS]"
+    exit 1
+fi
+
+OPTIONS=$1
+NAME_SITE=$2
+DB_NAME=$3
+DB_USER=$4
+DB_PASS=$5
+
+# Check if the user has provided a valid option
+if [[ "$1" != "-u" && "$1" != "--user" && "$1" != "-p" && "$1" != "--post" && "$1" != "-f" && "$1" != "--forminator" && "$1" != "-c" && "$1" != "--contest" && "$1" != "-a" && "$1" != "--all" && "$1" != "-h" && "$1" != "--help" ]]; then
+    echo "Error: Invalid option. Use -u, --user, -p, --post, -a or --all, -h or --help."
+    exit 1
+fi
+
+# Create directory in mysql-files if it doesn't exist
+if [ ! -d "/var/lib/mysql-files/$NAME_SITE" ]; then
+    echo "Creating directory /var/lib/mysql-files/$NAME_SITE"
+    mkdir -p /var/lib/mysql-files/$NAME_SITE
+fi
+if [ ! -d "/var/lib/mysql-files/$NAME_SITE/forminator" ]; then
+    echo "Creating directory /var/lib/mysql-files/$NAME_SITE/forminator"
+    mkdir -p /var/lib/mysql-files/$NAME_SITE/forminator
+fi
+if [ ! -d "/var/lib/mysql-files/$NAME_SITE/contest" ]; then
+    echo "Creating directory /var/lib/mysql-files/$NAME_SITE/contest"
+    mkdir -p /var/lib/mysql-files/$NAME_SITE/contest
+fi
+
+# Check if the CSV files exist
+if [ "$OPTIONS" == "-u" ] || [ "$OPTIONS" == "--user" ] || [ "$OPTIONS" == "-a" ] || [ "$OPTIONS" == "--all" ]; then
+    if [ ! -f "./$NAME_SITE/old_users.csv" ]; then
+        echo "Error: old_users.csv file not found in /$NAME_SITE"
+        exit 1
+    else
+        echo "old_users.csv file found in /$NAME_SITE"
+
+        cp ./$NAME_SITE/old_users.csv /var/lib/mysql-files/$NAME_SITE
+    fi
+    if [ ! -f "./$NAME_SITE/usermeta.csv" ]; then
+        echo "Error: old_users.csv file not found in /$NAME_SITE"
+        exit 1
+    else
+        echo "usermeta.csv file found in /$NAME_SITE"
+    fi
+fi
+if [ "$OPTIONS" == "-p" ] || [ "$OPTIONS" == "--post" ] || [ "$OPTIONS" == "-a" ] || [ "$OPTIONS" == "--all" ]; then
+    if [ ! -f "./$NAME_SITE/old_posts.csv" ]; then
+        echo "Error: wp_posts.csv file not found in /$NAME_SITE"
+        exit 1
+    else
+        echo "old_posts.csv file found in /$NAME_SITE"
+    fi
+    if [ ! -f "./$NAME_SITE/postmeta.csv" ]; then
+        echo "Error: wp_posts.csv file not found in /$NAME_SITE"
+        exit 1
+    else
+        echo "postmeta.csv file found in /$NAME_SITE"
+    fi
+fi
+if [ "$OPTIONS" == "-f" ] || [ "$OPTIONS" == "--forminator" ] || [ "$OPTIONS" == "-a" ] || [ "$OPTIONS" == "--all" ]; then
+    if [ ! -f "./$NAME_SITE/forminator/old_views.csv" ]; then
+        echo "Error: old_views.csv file not found in /$NAME_SITE/forminator"
+        exit 1
+    else
+        echo "old_views.csv file found in /$NAME_SITE/forminator"
+    fi
+    if [ ! -f "./$NAME_SITE/forminator/old_reports.csv" ]; then
+        echo "Error: old_reports.csv file not found in /$NAME_SITE/forminator"        
+        exit 1
+    else
+        echo "old_reports.csv file found in /$NAME_SITE/forminator"
+
+        cp ./$NAME_SITE/forminator/old_reports.csv /var/lib/mysql-files/$NAME_SITE
+    fi
+    if [ ! -f "./$NAME_SITE/forminator/old_entry.csv" ]; then
+        echo "Error: old_entry.csv file not found in /$NAME_SITE/forminator"
+        exit 1
+    else
+        echo "old_entry.csv file found in /$NAME_SITE/forminator"
+        
+        cp ./$NAME_SITE/forminator/old_entry.csv /var/lib/mysql-files/$NAME_SITE
+    fi
+    if [ ! -f "./$NAME_SITE/forminator/old_entry_meta.csv" ]; then
+        echo "Error: old_entry_meta.csv file not found in /$NAME_SITE/forminator"
+        exit 1
+    else
+        echo "old_entry_meta.csv file found in /$NAME_SITE/forminator"
+    fi
+fi
+
+echo "Change permissions for /var/lib/mysql-files"
+chmod -R 777 /var/lib/mysql-files
+ls -la /var/lib/mysql-files/cegsksae
+
+rm -rf /var/lib/mysql-files/$NAME_SITE/contest/new_posts.csv
+
+# Execute the php script
+php add_bdd.php $OPTIONS $NAME_SITE $DB_NAME $DB_USER $DB_PASS
+
+chmod -R 700 /var/lib/mysql-files
